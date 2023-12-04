74de68a632750278565204fdf0f41d5996cf217f
Osamaalsisi <12oa0312@amal.se>
2023-12-04 09:48:55 +0100

    Update Program.cs

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Write down your to do List:");
-string input = Console.ReadLine();
-Console.WriteLine(input);
+
+string input1 = Console.ReadLine(); 
+string input2 = Console.ReadLine();
+string input3 = Console.ReadLine();
